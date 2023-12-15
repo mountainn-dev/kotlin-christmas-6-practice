9a7ac19fb0092fd6f59d0321e9a6c5574232be97
seongsan <tjdtks030@naver.com>
2023-12-15 22:56:06 +0900

    feat: 총 주문 금액 10000 원 미만 시 프로모션 미적용

--- src/main/kotlin/christmas/controller/PromotionController.kt
+++ src/main/kotlin/christmas/controller/PromotionController.kt
@@ -1,4 +1,17 @@
 package christmas.controller
 
+import christmas.domain.Event.*
+import christmas.domain.Order
+import christmas.domain.Promotion
+import christmas.domain.VisitDay
+
 class PromotionController {
+    private val promotions = mutableListOf<Promotion>()
+
+
+    fun apply(visitDay: VisitDay, orders: List<Order>) {
+        if (orders.sumOf { it.total() } > 10000) applyPromotion(visitDay, orders)
+    }
+
+
 }
--- src/main/kotlin/christmas/domain/Promotion.kt
+++
@@ -1,4 +0,0 @@
-package christmas.domain
-
-enum class Promotion {
-}
