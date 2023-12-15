3d6cac599ea9b98dd1f53ca0e1da39ed3e602cbf
seongsan <tjdtks030@naver.com>
2023-12-15 21:06:10 +0900

    feat: 방문 날짜가 캘린더상 어느 날에 속하는지 확인 가능
    Calendar enum class 작성 및 contains() 구현
    VisitDay 클래스 isWeekDay, isWeekEnd, isSpecial() 구현

--- src/main/kotlin/christmas/domain/Calendar.kt
+++ src/main/kotlin/christmas/domain/Calendar.kt
@@ -1,4 +1,9 @@
 package christmas.domain
 
-enum class Calendar {
+enum class Calendar(private val days: List<Int>) {
+    WEEKDAY(listOf(3,4,5,6,7,10,11,12,13,14,17,18,19,20,21,24,25,26,27,28,31)),
+    WEEKEND(listOf(1,2,8,9,15,16,22,23,29,30)),
+    SPECIAL(listOf(3,10,17,24,25,31));
+
+    fun contains(day: Int) =  days.contains(day)
 }
--- src/main/kotlin/christmas/domain/VisitDay.kt
+++ src/main/kotlin/christmas/domain/VisitDay.kt
@@ -1,8 +1,16 @@
 package christmas.domain
 
+import christmas.domain.Calendar.*
+
 class VisitDay(private val day: Int) {
 
     init {
         require(day in 1..31)
     }
+
+    fun isWeekDay() = WEEKDAY.contains(day)
+
+    fun isWeekEnd() = WEEKEND.contains(day)
+
+    fun isSpecial() = SPECIAL.contains(day)
 }
