1dfae94e587d793183772919e975d1be3701c92b
seongsan <tjdtks030@naver.com>
2023-12-15 22:57:55 +0900

    feat: 주말 방문 시 주말 할인 이벤트 적용

--- src/main/kotlin/christmas/controller/PromotionController.kt
+++ src/main/kotlin/christmas/controller/PromotionController.kt
@@ -15,13 +15,22 @@ class PromotionController {
 
     private fun applyPromotion(visitDay: VisitDay, orders: List<Order>) {
         if (visitDay.isWeekDay()) weekDayPromotion(orders)
+        if (visitDay.isWeekEnd()) weekEndPromotion(orders)
     }
-    
+
     private fun weekDayPromotion(orders: List<Order>) {
         var count = 0
 
         orders.map { if (it.isDessert()) count += it.count() }
 
         promotions.add(Promotion(WEEKDAY_EVENT, WEEKDAY_EVENT.value() * count))
+    }
+
+    private fun weekEndPromotion(orders: List<Order>) {
+        var count = 0
+
+        orders.map { if (it.isMainDish()) count += it.count() }
+
+        promotions.add(Promotion(WEEKEND_EVENT, WEEKEND_EVENT.value() * count))
     }
 }
